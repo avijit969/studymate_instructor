7777518e4b5098f413647be23034219bc1773a9e
Avijit <abhijitpradhan909@gmail.com>
2025-07-08 21:15:56 +0530

    fix: update middleware route matcher to include only specific routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -47,5 +47,5 @@ export default async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  matcher: ["/", "/login", "/signup"],
 };
